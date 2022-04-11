92ba1188626db1df50d9b83363e0afd96c699600
ssunny85 <ssssunny85@gmail.com>
2022-04-11 11:29:48 +0900

    cypress 패키지 설치 및 sample 테스트 코드 작성

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,12 @@ module.exports = {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
   },
-  extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'prettier',
+    'plugin:cypress/recommended',
+  ],
   plugins: [],
   // add your custom rules here
   rules: {},
